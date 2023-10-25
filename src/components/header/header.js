f79b1a296bec5c1facd13fe7da58016ae714d8d5
Lev Rednikov <120374210+CheateRYT@users.noreply.github.com>
2023-10-25 21:05:05 +0300

    Header Added

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,9 +1,22 @@
-const Header = (props) => {
-  return (
-   <header className="header">
-    Privet
-   </header>
-  );
+import React from "react";
+import { Component } from "react";
+import logo from "../../img/Header-logo.png";
+
+class Header extends Component {
+  render() {
+    return (
+        <header className="header">
+      <img src={logo} className="header__logo" alt="logo" />
+          <nav className="nav">
+            <ul className="nav__list">
+              <li className="nav__item"><a className="nav__link" href="">Упражнения</a></li>
+              <li className="nav__item"><a className="nav__link" href="">Прогрессия</a></li>
+              <li className="nav__item"><a className="nav__link" href="">Профиль</a></li>
+            </ul>
+          </nav>
+        </header>
+    );
   }
-  
-  export default Header;
+}
+
+export default Header;

f78d9e6ba5ab63213fa48f9594b7b77273ccb192
Lev Rednikov <120374210+CheateRYT@users.noreply.github.com>
2023-10-25 14:36:33 +0300

    fixes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.min.css';
-import App from './components/app/App';
+import App from './components/app/app';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-<span className='govno'>Govno</span>
+    <App />
   </React.StrictMode>
 );
